c1d214c817097d76fd4aee56461c317bf5f0bcb5
Yiliang Yuan <yyuan2696@gmail.com>
2022-11-30 21:57:14 -0500

    fix 2(d). Add a train in 3a

--- CVmaster.R
+++ CVmaster.R
@@ -7,10 +7,10 @@ CVmaster <- function(gen_c,train_feature,train_label,K=10,loss_function){
   df = cbind(train_label,train_feature)
   df$expert_label = as.factor(df$expert_label)
   tr_method = trainControl(method = "cv", number = 10, savePredictions=TRUE)
-  fit_train = train(form, data=df, method=gen_c, trControl = tr_method)
+  if(gen_c == "glm")
+    fit_train = train(form, data=df, method=gen_c, trControl = tr_method, family="binomial")
+  else
+    fit_train = train(form, data=df, method=gen_c, trControl = tr_method)
   pred_result <- fit_train$pred
-  obs <- pred_result$obs
-  pred <- pred_result$pred
-  pred_result$equal <- ifelse(pred == obs, 1,0)
-  loss_function(pred,obs)
+  loss_function(pred_result)
 }
