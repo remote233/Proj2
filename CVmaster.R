6c729f7a9f880d61ab72183039193ac19d984f86
Yiliang Yuan <yyuan2696@gmail.com>
2022-12-02 02:09:00 -0500

    new version CVmaster. still need SVM and adboost model

--- CVmaster.R
+++ CVmaster.R
@@ -1,18 +1,84 @@
-
-library(tidyverse) 
+library(tidyverse)
 library(caret)
+library(e1071)
+library(MASS)
+library(class)
 
-CVmaster <- function(gen_c,train_feature,train_label,K=10,loss_function){
+# helper functions
+lr_model <- function(formula, folds, K){
+  accuracy = matrix(NA,ncol=2,nrow=K)
+  colnames(accuracy) <- c("folds","accuracy")
+  accuracy[,1] = sapply(1:K, function(i) paste("folds",i,sep=""))
+  for(i in 1:K){
+    validation = folds %>% filter(folds==i)
+    train = folds %>% filter(folds!=i)
+    train_logit = glm(formula,data=train,family = "binomial")
+    pred_prob = predict(train_logit, validation, type="response")
+    pred = rep(0,length(pred_prob))
+    pred[pred_prob>0.5] = 1
+    accuracy[i,2] = round(mean(pred==validation$expert_label),3)
+  }
+  return(accuracy)
+}
+
+nb_model <- function(formula, folds, K){
+  accuracy = matrix(NA,ncol=2,nrow=K)
+  colnames(accuracy) <- c("folds","accuracy")
+  accuracy[,1] = sapply(1:K, function(i) paste("folds",i,sep=""))
+  for(i in 1:K){
+    validation = folds %>% filter(folds==i)
+    train = folds %>% filter(folds!=i)
+    train_nb = naiveBayes(formula,data=train)
+    pred = predict(train_nb, validation)
+    accuracy[i,2] = round(mean(pred==validation$expert_label),3)
+  }
+  return(accuracy)
+}
+
+qda_model <- function(formula, folds, K){
+  accuracy = matrix(NA,ncol=2,nrow=K)
+  colnames(accuracy) <- c("folds","accuracy")
+  accuracy[,1] = sapply(1:K, function(i) paste("folds",i,sep=""))
+  for(i in 1:K){
+    validation = folds %>% filter(folds==i)
+    train = folds %>% filter(folds!=i)
+    train_qda = qda(formula,data=train)
+    pred = predict(train_qda, validation)
+    accuracy[i,2] = round(mean(pred$class==validation$expert_label),3)
+  }
+  return(accuracy)
+}
+
+lda_model <- function(formula, folds, K){
+  accuracy = matrix(NA,ncol=2,nrow=K)
+  colnames(accuracy) <- c("folds","accuracy")
+  accuracy[,1] = sapply(1:K, function(i) paste("folds",i,sep=""))
+  for(i in 1:K){
+    validation = folds %>% filter(folds==i)
+    train = folds %>% filter(folds!=i)
+    train_lda = lda(formula,train)
+    pred = predict(train_lda, validation)
+    accuracy[i,2] = round(mean(pred$class==validation$expert_label),3)
+  }
+  return(accuracy)
+}
+
+knn_model <- function(formula, folds, K){
+  accuracy = matrix(NA,ncol=2,nrow=K)
+  colnames(accuracy) <- c("folds","accuracy")
+  accuracy[,1] = sapply(1:K, function(i) paste("folds",i,sep=""))
+  for(i in 1:K){
+    validation = folds %>% filter(folds==i)
+    train = folds %>% filter(folds!=i)
+    knn_pred = knn(train[,c("NDAI","SD","CORR")], validation[,c("NDAI","SD","CORR")], train$expert_label, k=10)
+    accuracy[i,2] = round(mean(knn_pred==validation$expert_label),3)
+  }
+  return(accuracy)
+}
+
+CVmaster <- function(gen_model, train_feature, train_label, train_folds, K=10){
   form = as.formula(paste(colnames(train_label), paste(colnames(train_feature),collapse = "+"),sep = "~"))
-  df = cbind(train_label,train_feature)
-  df$expert_label = as.factor(df$expert_label)
-  
-  tr_method = trainControl(method = "cv", number = K, savePredictions=TRUE)
-  if(gen_c == "glm")
-    fit_train = train(form, data=df, method=gen_c, trControl = tr_method, family="binomial")
-  else
-    fit_train = train(form, data=df, method=gen_c, trControl = tr_method)
-  pred_result <- fit_train$pred
-  loss_function(pred_result)
+  folds = cbind(train_feature, train_label, train_folds)
+  gen_model(form, folds, K)
 }
 
